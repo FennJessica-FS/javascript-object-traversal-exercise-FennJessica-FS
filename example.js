a8fdb1254ccc40cd0fe0dca327117ca1fd525ed1
brandonbrown <brandon@centraflux.com>
2022-02-27 10:40:18 -0500

    example activity

---
+++ example.js
@@ -0,0 +1,14 @@
+'use strict';
+(() => {
+    const exampleJavaScriptObject = {
+        course: 'Application Development',
+        school: 'Full Sail University',
+        degree: 'Web Development'
+    }
+
+    const school = exampleJavaScriptObject.school;
+    const degree = exampleJavaScriptObject.degree;
+    const course = exampleJavaScriptObject.course;
+
+    console.log(`I am a student at ${school}, in the ${degree} degree, currently taking ${course}.`)
+})()
--- main.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-(() => {
-    // Code Here
-})()
